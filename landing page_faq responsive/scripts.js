29e4b421c06adeea33c30ba17829d4e3561d5a88
roanek jena <84308970+rj-debug@users.noreply.github.com>
2023-11-01 19:34:53 +0530

    Add files via upload
    Sqaure box FAQ's

--- landing page_faq responsive/scripts.js
+++ landing page_faq responsive/scripts.js
@@ -6,11 +6,13 @@ function toggleAnswer(button) {
     if (faqItem.classList.contains('open')) {
         // If the FAQ item is open, close it
         answer.style.height = '0';
+        answer.style.minHeight = '0'; // set min-height to zero
         answerContent.style.overflow = 'hidden';
         faqItem.classList.remove('open');
     } else {
         // If the FAQ item is closed, open it
         answer.style.height = answerContent.scrollHeight + 'px';
+        answer.style.minHeight = '100px'; // set min-height to the minimum value
         answerContent.style.overflow = 'visible';
         faqItem.classList.add('open');
     }
